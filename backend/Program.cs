1f8ec2d333209f1fefed73102703fa5fd601da8c
jacenielson <jace.nielson@students.snow.edu>
2026-03-31 14:51:24 -0600

    styling movie grid

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,9 +5,9 @@ app.UseCors();
 
 app.MapGet("/", () => "Hello World!");
 List<Movie> movieList = [
-new Movie ("101", "Super Mario Galaxy", "posters/supermario.jpg", ["Animation", "Adventure", "Fantasy"], 4.9, "Travel across the cosmos to rescue Princess Peach from the clutches of Bowser in this gravity-defying adventure.", ["10:00 AM", "1:00 PM", "4:30 PM"], true),
-new Movie ("102", "Project Hail Mary", "posters/projecthailmary.jpg", ["Sci-Fi", "Drama"], 4.7, "Ryland Grace is the sole survivor on a desperate, last-chance mission to save humanity and the Earth itself.", ["2:00 PM", "5:30 PM", "9:00 PM"], true),
-new Movie ("103", "Spider-Man: Brand New Day", "posters/spiderman.webp", ["Action", "Sci-Fi", "Adventure"], 4.0, "Peter Parker navigates a clean slate in New York City, facing fresh villains and a complicated social life.", ["12:45 PM", "4:00 PM", "8:15 PM", "10:30 PM"], true)
+new Movie ("101", "Super Mario Galaxy", "Images/supermario.jpg", ["Animation", "Adventure", "Fantasy"], 4.9, "Travel across the cosmos to rescue Princess Peach from the clutches of Bowser in this gravity-defying adventure.", ["10:00 AM", "1:00 PM", "4:30 PM"], true),
+new Movie ("102", "Project Hail Mary", "Images/hailmary.jpg", ["Sci-Fi", "Drama"], 4.7, "Ryland Grace is the sole survivor on a desperate, last-chance mission to save humanity and the Earth itself.", ["2:00 PM", "5:30 PM", "9:00 PM"], true),
+new Movie ("103", "Spider-Man: Brand New Day", "Images/spiderman.jpg", ["Action", "Sci-Fi", "Adventure"], 4.0, "Peter Parker navigates a clean slate in New York City, facing fresh villains and a complicated social life.", ["12:45 PM", "4:00 PM", "8:15 PM", "10:30 PM"], true)
 ];
 app.MapGet("/movies", () =>
 {
--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443ae79cee5af4dac90c40b8b09bbc567646d00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee77aa4cd71ee0527bf41280a9f17aabeffe4e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
